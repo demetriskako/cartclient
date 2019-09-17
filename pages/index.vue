124ebaf38563e66ae859c2dddc15418523ec849f
Dimitris <Dimitris>
2019-09-17 17:14:28 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="section">
+        <div class="container is-fluid">
+            <p>Home</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+        }
+    }
+</script>
+
+<style>
+</style>
